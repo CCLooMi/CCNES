8f0943b419e53fed2f8016a12b7a62e92d87662b
chenxianjun <1004178387@qq.com>
2017-10-13 12:26:49 +0800

    代码优化
    代码优化

--- src/main/java/com/ccnes/websocket/BaseWebSocketServer.java
+++ src/main/java/com/ccnes/websocket/BaseWebSocketServer.java
@@ -1,9 +1,6 @@
 package com.ccnes.websocket;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
@@ -11,84 +8,62 @@ import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
-
-import com.ccnes.protocol.NesProtocol;
 /**带@on*的任何一个方法内部出现异常都会导致session关闭，所以所有方法中最好tryCatch捕获所有异常*/
 public abstract class BaseWebSocketServer {
 
-	protected static final NesProtocol np=new NesProtocol();
-	protected static final Map<String, Session>SESSIONS=new ConcurrentHashMap<>();
-	
 	protected Session session;
-	protected Session toSession;
-	protected String uid;
-	protected String tuid;
 	
 	//注意子类重写这些方法之后将取不到这些方法上面的注解
 	
 	@OnOpen
 	public void onOpen(Session session){
 		this.session=session;
+		try{
+			this.whenOpen(session);
+		}catch (Exception e) {
+			this.onError(session, e);
+		}
 	};
 	
 	@OnMessage
 	public void onTextMessage(String msg){
-		System.out.println("Got Text Message\t"+msg);
+		try{
+			this.receiveTextMessage(msg);
+		}catch (Exception e) {
+			this.onError(session, e);
+		}
 	};
 	
 	@OnMessage
 	public void onBinaryMessage(ByteBuffer bb){
 		try{
-			byte[]bs=bb.array();
-			Map<String, Object>m;
-			switch (bs[0]) {
-			case 10://login
-				m=np.converMsgToMap(bs);
-				this.uid=(String) m.get("uid");
-				if(uid!=null){
-					SESSIONS.put(uid, session);
-					System.out.println("uid:\t["+uid+"] login successed.");
-				}
-				break;
-			case 20://bindHandle
-				m=np.converMsgToMap(bs);
-				this.tuid=(String)m.get("tuid");
-				if(this.tuid!=null){
-					this.toSession=SESSIONS.get(tuid);
-					System.out.println("handle:\t["+uid+"] bind to ["+tuid+"] successed.");
-				}
-				break;
-			default:
-				if(toSession!=null&&toSession.isOpen()){
-					//不使用异步方式，容易出现数据还在发送另一个又来了，导致出现异常
-					toSession.getBasicRemote().sendBinary(bb);
-				}else if(tuid!=null){
-					toSession=SESSIONS.get(tuid);
-				}
-				break;
-			}
+			this.receiveBinaryMessage(bb);
 		}catch (Exception e) {
-			e.printStackTrace();
+			this.onError(session, e);
 		}
 	};
 	
 	@OnClose
 	public void onClose(CloseReason closeReason){
-		System.out.println("Session closed["+closeReason.getCloseCode()+"]:\t"+closeReason.getReasonPhrase());
-		if(uid!=null){
-			SESSIONS.remove(uid);
+		try{
+			this.whenClose(closeReason);
+		}catch (Exception e) {
+			this.onError(session, e);
 		}
 	};
 	
 	@OnError
 	public void onError(Session session, Throwable error){
-		if(uid!=null){
-			SESSIONS.remove(uid);
-		}
 		try {
+			this.whenError(session, error);
 			this.session.close();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	};
+	public abstract void whenOpen(Session session);
+	public abstract void whenClose(CloseReason closeReason);
+	public abstract void whenError(Session session, Throwable error);
+	public abstract void receiveTextMessage(String msg) throws Exception;
+	public abstract void receiveBinaryMessage(ByteBuffer bb) throws Exception;
 }
--- src/main/java/com/ccnes/websocket/CenterServer.java
+++ src/main/java/com/ccnes/websocket/CenterServer.java
@@ -1,8 +1,16 @@
 package com.ccnes.websocket;
 
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.websocket.CloseReason;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
+
+import com.ccnes.protocol.NesProtocol;
 
 //非embed的wen容器使用SpringConfigurator
 //@ServerEndpoint(value="/ccnes",configurator=SpringConfigurator.class)
@@ -10,5 +18,70 @@ import org.springframework.stereotype.Component;
 @Component
 @ServerEndpoint(value="/ccnes")
 public class CenterServer extends BaseWebSocketServer{
+	protected static final NesProtocol np=new NesProtocol();
+	protected static final Map<String, Session>SESSIONS=new ConcurrentHashMap<>();
+
+	protected Session toSession;
+	protected String uid;
+	protected String tuid;
+	
+	@Override
+	public void whenOpen(Session session) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void whenClose(CloseReason closeReason) {
+		System.out.println("Session closed["+closeReason.getCloseCode()+"]:\t"+closeReason.getReasonPhrase());
+		if(uid!=null){
+			SESSIONS.remove(uid);
+		}
+	}
+
+	@Override
+	public void whenError(Session session, Throwable error) {
+		if(uid!=null){
+			SESSIONS.remove(uid);
+		}
+	}
+
+	@Override
+	public void receiveTextMessage(String msg) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveBinaryMessage(ByteBuffer bb) throws Exception {
+		byte[]bs=bb.array();
+		Map<String, Object>m;
+		switch (bs[0]) {
+		case 10://login
+			m=np.converMsgToMap(bs);
+			this.uid=(String) m.get("uid");
+			if(uid!=null){
+				SESSIONS.put(uid, session);
+				System.out.println("uid:\t["+uid+"] login successed.");
+			}
+			break;
+		case 20://bindHandle
+			m=np.converMsgToMap(bs);
+			this.tuid=(String)m.get("tuid");
+			if(this.tuid!=null){
+				this.toSession=SESSIONS.get(tuid);
+				System.out.println("handle:\t["+uid+"] bind to ["+tuid+"] successed.");
+			}
+			break;
+		default:
+			if(toSession!=null&&toSession.isOpen()){
+				//不使用异步方式，容易出现数据还在发送另一个又来了，导致出现异常
+				toSession.getBasicRemote().sendBinary(bb);
+			}else if(tuid!=null){
+				toSession=SESSIONS.get(tuid);
+			}
+			break;
+		}
+	}
 	
 }
